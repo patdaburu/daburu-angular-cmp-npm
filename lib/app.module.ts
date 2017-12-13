8a1e8507d1a293ac81139866ee9b3eb42ab1431d
patb <patb@stearnsbank.com>
2017-12-13 09:01:14 -0600

    Adding TypeDoc support.

---
+++ lib/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
+import { MySecondComponentComponent } from './components/my-second-component/my-second-component.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponentComponent,
+    MySecondComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
